a7c50552defb1b5248a791d24799cb1c5bf60fc1
Timothy-Simon <69052048+Timothy-Simon@users.noreply.github.com>
2023-08-25 21:47:26 +0530

    Delete public directory

--- public/js/header.js
+++
@@ -1,7 +0,0 @@
-const toggle = document.getElementsByClassName('toggle')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggle.addEventListener('click',()=>{
-    navbarLinks.classList.toggle('active');
-})
-    
